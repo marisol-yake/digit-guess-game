3ca9965df080337174e7ab9014781c8a8327574c
Ryan Yake <r.yake402@gmail.com>
2020-04-13 15:46:19 -0600

    Update draw_pad.py

--- draw_pad.py
+++ draw_pad.py
@@ -13,9 +13,10 @@ def main():
     running = True
 
     # This sets up the user-facing pygame display
-    screen = pygame.display.set_mode((640, 640))
+    window = pygame.display.set_mode((640, 800))
+    screen = pygame.Surface(size=(8, 8))
     pygame.display.set_caption('0 Thru 9: a guessing machine')
-    screen.fill(white)
+    window.fill(white)
     clock = pygame.time.Clock()
 
     while running:
@@ -24,19 +25,23 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
-            # Resets screen if R-key is pressed
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
-                    # num_image = pygame.pixelcopy.surface_to_array()
-                    # print(num_image)
+                    num_image = pygame.surfarray.array2d(screen)
+                    print(num_image)
+                    print(num_image.shape)
+
+                # Resets screen if R-key is pressed
                 elif event.key == pygame.K_r:
+                    window.fill(white)
+
                     screen.fill(white)
                     pygame.display.update()
 
             elif event.type == pygame.MOUSEMOTION:
                 if event.buttons[0]:
                     last = (event.pos[0] - event.rel[0], event.pos[1] - event.rel[1])
-                    pygame.draw.line(screen, black, last, event.pos, 16)
+                    pygame.draw.line(screen, black, last, event.pos, 3)
             pygame.display.update()
             clock.tick(30)
 
